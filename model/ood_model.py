fa2f313006338804e09ffdbb1ec08e2bdc0eb556
suzhenpeng02 <suzhenpeng02@meituan.com>
2022-04-23 22:29:04 +0800

    ood

--- model/ood_model.py
+++ model/ood_model.py
@@ -1,6 +1,6 @@
 from transformers.models.bert.modeling_bert import *
 from torch.nn import CrossEntropyLoss
-import torch.nn.functional as F
+
 logger = logging.get_logger(__name__)
 def soft_logits(input : torch.Tensor ,target : torch.Tensor ,mode:str="average",tmp: float = 1):
     input=torch.div(input,tmp)
@@ -16,61 +16,6 @@ def soft_logits(input : torch.Tensor ,target : torch.Tensor ,mode:str="average",
     else:
         raise ValueError("loss mode error")
 
-class classification_moudle(nn.Module):
-    def __init__(self,hidden_size):
-        super(classification_moudle,self).__init__()
-        self.hidden_size=hidden_size
-        self.linear1=nn.Linear(self.hidden_size,2*self.hidden_size)
-        self.linear2=nn.Linear(2*self.hidden_size,self.hidden_size)
-    def forward(self,x):
-        y=x
-        y=self.linear1(y)
-        y=F.leaky_relu(y)
-        y=F.normalize(y)
-        y=F.dropout(y,p=0.5)
-        y=self.linear2(y)
-        y=F.leaky_relu(y)
-        y=F.normalize(y)
-        x=x+y#残差链接
-        return x
-
-class classification_model(nn.Module):
-    def __init__(self,hidden_size,output_size):
-        super(classification_model,self).__init__()
-        self.hidden_size,self.output_size=hidden_size,output_size
-        self.moudle1=classification_moudle(self.hidden_size)
-        self.moudle2=classification_moudle(self.hidden_size)
-        self.linear1=nn.Linear(self.hidden_size,int(self.hidden_size/2))
-        self.linear2=nn.Linear(int(self.hidden_size/2),self.output_size)
-    def forward(self,x):
-        y=self.moudle1(x)
-        y=self.moudle2(y)
-        y=self.linear1(y)
-        y=F.leaky_relu(y)
-        y=F.normalize(y)
-        y=self.linear2(y)
-        return y
-
-class binary_model(nn.Module):
-    def __init__(self, hidden_size, output_size, softmax_size):
-        super(binary_model,self).__init__()
-        self.hidden_size, self.output_size,self.softmax_size = hidden_size, output_size,softmax_size
-        self.moudle1 = classification_moudle(self.hidden_size)
-        self.moudle2 = classification_moudle(self.hidden_size)
-        self.linear1 = nn.Linear(self.hidden_size+self.softmax_size, int(self.hidden_size / 2))
-        self.linear2 = nn.Linear(int(self.hidden_size / 2)+self.softmax_size, self.output_size)
-
-    def forward(self, x , soft_scores):
-        y = self.moudle1(x)
-        y = self.moudle2(y)
-        y = torch.cat([y,soft_scores],dim=1)
-        y = self.linear1(y)
-        y = F.leaky_relu(y)
-        y = F.normalize(y)
-        y = torch.cat([y,soft_scores],dim=1)
-        y = self.linear2(y)
-        return y
-
 class BertForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
@@ -82,9 +27,8 @@ class BertForSequenceClassification(BertPreTrainedModel):
             config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
         )
         self.dropout = nn.Dropout(classifier_dropout)
-        # self.classifier = nn.Linear(config.hidden_size, config.num_labels)
-        self.classifier = classification_model(config.hidden_size, config.num_labels)
-        self.binary_classifier=binary_model(2*config.hidden_size,2,config.num_labels)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
         # Initialize weights and apply final processing
         self.post_init()
 
@@ -98,14 +42,11 @@ class BertForSequenceClassification(BertPreTrainedModel):
         head_mask: Optional[torch.Tensor] = None,
         inputs_embeds: Optional[torch.Tensor] = None,
         labels: Optional[torch.Tensor] = None,
-        binary_labels: Optional[torch.Tensor]=None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         mode:Optional[str] = "train",
         use_soft:Optional[bool]=False,
-        beta:Optional[float]=1.0,
-        alpha:Optional[float]=1.0,
         tmp: Optional[float] = 1,
     ) :
         r"""
@@ -128,31 +69,40 @@ class BertForSequenceClassification(BertPreTrainedModel):
             return_dict=return_dict,
         )
 
-        hidden_states = outputs.hidden_states
-        last_layer_hidden_states=hidden_states[-1]
-        cls_hidden_state = last_layer_hidden_states[:, 0, :]
-        sep_hidden_state=last_layer_hidden_states[:, -1, :]
-        cls_hidden_state = self.dropout(cls_hidden_state)
-        sep_hidden_state = self.dropout(sep_hidden_state)
-        classify_logits=self.classifier(cls_hidden_state)
-        cls_copy=cls_hidden_state.clone().detach()
-        cls_copy.requires_grad=True
-        binary_logits=self.binary_classifier(torch.cat([cls_copy,sep_hidden_state],dim=1),classify_logits)
-        loss = None
-        if labels is not None and binary_labels is not None:
-            classify_loss_fct = CrossEntropyLoss(reduction='none')
-            binary_loss_fct = CrossEntropyLoss()
+        pooled_output = outputs[1]
 
-            classify_loss_seq = classify_loss_fct(torch.div(classify_logits.view(-1, self.num_labels), tmp),
-                                                  labels.view(-1))
-            pos_loss_seq = classify_loss_seq[(1 - binary_labels).bool()]
-            classify_loss = torch.div(torch.sum(pos_loss_seq), len(pos_loss_seq))
-            binary_loss = binary_loss_fct(binary_logits.view(-1, 2), binary_labels.view(-1))
-            loss = alpha * classify_loss + beta * binary_loss
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            loss_fct = CrossEntropyLoss()
+            if mode == "train":
+                if use_soft == False:
+                    loss = loss_fct(torch.div(logits.view(-1, self.num_labels),tmp), labels.view(-1))
+                else:
+                    loss =soft_logits(logits.view(-1, self.num_labels),labels.view(-1, self.num_labels),tmp=tmp)
+                    # loss = loss_fct(torch.div(logits.view(-1, self.num_labels),tmp), labels.view(-1, self.num_labels))
+            elif mode == "val" or mode == "test":
+                # loss = loss_fct(torch.div(logits.view(-1, self.num_labels),tmp), labels.view(-1))
+                loss = -1
+            else:
+                raise ValueError("loss value error")
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
 
         return SequenceClassifierOutput(
             loss=loss,
-            logits=classify_logits,
+            logits=logits,
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
